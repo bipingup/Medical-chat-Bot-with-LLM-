ecc88b0cd1b0967709ef3a153fc112954a6443e9
bipingup <biping9930@gmail.com>
2025-12-31 14:24:46 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Bipin Gupta",
+    author_email="biping9930@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
